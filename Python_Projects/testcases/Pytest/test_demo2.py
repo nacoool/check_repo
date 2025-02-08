efb3d58f21f7d0c41a8f28d9e9f4a46662633922
Nakul Pathak <nakul.pathak@coriolis.co.in>
2025-02-09 00:56:08 +0530

    changing demo1 and demo2

--- Python_Projects/testcases/Pytest/test_demo2.py
+++ Python_Projects/testcases/Pytest/test_demo2.py
@@ -8,10 +8,9 @@ def fixture1():
     yield count1  # Corrected "yield"
     print("this is a fixture teardown")
 
-@pytest.mark.parametrize("num2", [3, 4])  #  Corrected "parametrize"
-@pytest.mark.parametrize("num1", [1, 2])  # Placed in correct order
+@pytest.mark.parametrize("num1", [1, 2])
+@pytest.mark.parametrize("num2", [3, 4])
 def test_make_calc(fixture1, num1, num2):
-    import pdb; pdb.set_trace()
     print(f"fixture1: {fixture1}, num1: {num1}, num2: {num2}")
     assert True
 
